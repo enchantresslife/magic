c518114cd84b6263cd87a6ea2bedd9d4f74225e2
enchantresslife <63981431+enchantresslife@users.noreply.github.com>
2020-04-19 17:54:34 -0600

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,19 @@
+let Mabel = sprites.create(img`
+. . . . . . . . . . . . . . . . 
+. . . . . . f f f f f . . . . . 
+. . . . . f e e e e e f . . . . 
+. . . . f e e 9 9 9 e e f . . . 
+. . . . f e 9 f f f 9 e f . . . 
+. . . . f 9 f f d f f 9 f . . . 
+. . . f e f d e d e d f e f . . 
+. . . f e f 3 d d d 3 f e f . . 
+. . . f e e f d d d f e e f . . 
+. . f e e f 9 f f f 9 f e e f . 
+. . . f f 9 9 9 9 9 9 9 f f . . 
+. . . f d d f 9 9 9 f d d f . . 
+. . . . f f f 9 9 9 f f f . . . 
+. . . . . . f 2 2 2 f . . . . . 
+. . . . . . f f f f f . . . . . 
+. . . . . . f 1 f 1 f . . . . . 
+. . . . . . f f f f f . . . . . 
+`, SpriteKind.Player)
